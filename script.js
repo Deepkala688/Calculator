6d09aba8ecf7ae54509158b95d0ff389db01e084
Deepkala688 <dipikashrestha955@gmail.com>
2025-07-31 07:28:41 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var age = 25;
+var age = 24;
+console.log(age);
+
+let name="Jenny";
+name="Jeshmi";
+console.log(name);
+
+const student = {
+    fullname:"Jenny Rajak",
+    age:"30",
+    address:"Naxal",
+}
+console.log(student);
+
+const attendance = {
+    name : "sita",
+    group : "4",
+    course : "computer",
+    percentage : "70%",
+    isPresent : true,
+}
+console.log(attendance);
